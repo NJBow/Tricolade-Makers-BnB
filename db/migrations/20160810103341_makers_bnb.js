99fee1029c65ac48209d88c1b0e9cab8cb7ddd51
yurizhuravel <zhuravel@gmail.com>
2016-08-12 11:32:06 +0100

    Adds and seeds users table

--- db/migrations/20160810103341_makers_bnb.js
+++ db/migrations/20160810103341_makers_bnb.js
@@ -1,12 +1,30 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('spaces', function(table){
-    table.increments();
-    table.string('name').notNullable().unique();
-    table.string('description', 500).notNullable();
-    table.integer('price_per_night').notNullable();
-  });
+
+  return Promise.all([
+
+    knex.schema.createTable('users', function(table){
+      table.increments();
+      table.string('name').notNullable().unique();
+      table.string('email').notNullable().unique();
+      table.string('password').notNullable();
+    }),
+
+    knex.schema.createTable('spaces', function(table){
+      table.increments();
+      table.string('name').notNullable().unique();
+      table.string('description', 500).notNullable();
+      table.integer('price_per_night').notNullable();
+    })
+
+  ]);
+
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('spaces');
+
+  return Promise.all([
+    knex.schema.dropTable('spaces'),
+    knex.schema.dropTable('users')
+  ]);
+
 };
